29114bd6905801201f97ade6935d44835bd5f01b
SEXYJungmin <jmdev@kakao.com>
2020-11-27 20:03:51 +0900

    10172 개(2020/11/27)

---
+++ 10000/10172.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    printf("|\\_/|\n");
+    printf("|q p|   /}\n");
+    printf("( 0 )""\"\"\"\\\n");
+    printf("|""\"^""\"""`    |\n");
+    printf("||_/=\\\\__|");
+
+    return 0;
+}
