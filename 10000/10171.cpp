516444a3a0db695b4d408ba2d5b13618210dbec8
SEXYJungmin <jmdev@kakao.com>
2020-11-27 19:57:49 +0900

    10171 고양이(2020/11/27)

---
+++ 10000/10171.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    printf("\\    /\\\n");
+    printf(" )  ( ')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|""\n");
+
+    return 0;
+}
