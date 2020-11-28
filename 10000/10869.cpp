651fc29e7c043f05eca9e721c5743f1207df6e00
SEXYJungmin <jmdev@kakao.com>
2020-11-28 20:41:52 +0900

    10869 사칙연산(2020/11/28)

---
+++ 10000/10869.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a, b;
+    scanf("%d%d", &a, &b);
+
+    printf("%d\n%d\n%d\n%d\n%d\n", a+b, a-b, a*b, a/b, a%b);
+
+    return 0;
+}
