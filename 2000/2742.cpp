56ff4493b21bb5bb505d4819e0ac4635eb9645fe
SEXYJungmin <jmdev@kakao.com>
2020-11-29 23:53:21 +0900

    2742 기찍 N(2020/11/29)

---
+++ 2000/2742.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    scanf("%d", &n);
+
+    for(int i = n; i >= 1; i --){
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
