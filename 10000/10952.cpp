7d534c9154a738b32152b5e1de78c468ace7a03b
SEXYJungmin <jmdev@kakao.com>
2020-12-06 21:25:43 +0900

    10952 A+B - 5(2020/12/06)

---
+++ 10000/10952.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a, b;
+    while (1){
+        scanf("%d%d", &a, &b);
+
+        if(a == 0 && b == 0){
+            break;
+        }
+        
+        printf("%d\n", a + b);
+    }
+
+    return 0;
+}
